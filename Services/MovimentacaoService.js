3a62f05780148e1fc67639bde628df6c809d64c3
Micael <micaelbga@gmail.com>
2023-05-17 18:09:42 -0300

    retirada do end() dos services

--- Services/MovimentacaoService.js
+++ Services/MovimentacaoService.js
@@ -22,26 +22,25 @@ class MovimentacaoService{
             
             db.query("SELECT * from movimentacao WHERE cartao_id = (?)",(err,result)=>{
                 if(err){
-                    throw err
+                    result = res.status(500).json(err);
                 }else{
                     response = res.status(200).json(result);
                 }
             });
-            db.end();
             return result;
         }
 
         static postMovimentacao(req,res){
             let nfcCod = Object.keys(req.body)[0];
             let date = new Date();
+            let result;
             db.query("INSERT INTO movimentacao (data_movimentacao, cartao_id) values (?,?)",[date,nfcCod],(err,result)=>{
                 if(err){
-                    throw err
+                    result = res.status(500).json(err);
                 }else{
                     result = res.status(200).json(result);
                 }
             });
-            db.end();
             return result;
         }
 }
